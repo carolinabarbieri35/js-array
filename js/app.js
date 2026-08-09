db341b5d9ccf04d6b8d0e188f50f41d03891489e
Carolina Barbieri <carolinabarbieri35@gmail.com>
2026-08-09 18:08:04 +0200

    esercizio-2

--- js/app.js
+++ js/app.js
@@ -8,3 +8,14 @@ for (let i = 1; i <= 10; i++) {
     console.log(i);
 
 }
+
+// stampa i numeri da 1 a 10 usando un ciclo while
+
+let i = 1
+
+while (i <= 10) {
+    console.log(i);
+
+    i++;
+
+}

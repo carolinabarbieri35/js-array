449fc62fa04f50acd22e600464e31bd9da45733d
Carolina Barbieri <carolinabarbieri35@gmail.com>
2026-08-17 12:44:16 +0200

    array 2

--- js/array.js
+++ js/array.js
@@ -20,3 +20,26 @@ for (let i = 0; i <= 6; i++) {
     }
 } console.log(numeri);
 
+// Dato l'array di numeri [10, 21, 6, 73, 9, 14, 44], crea un array con soli numeri pari
+
+const numbers = [10, 21, 6, 73, 9, 14, 44];
+
+const numeriPari = [];
+
+for (let i = 0; i < numbers.length; i++) {
+
+
+    if (numbers[i] % 2 === 0) {
+
+        numeriPari.push(numbers[i]);
+
+
+    }
+
+}
+console.log(numeriPari);
+
+// Dato un array di temperature settimanali [19, 21, 18, 23, 25, 22, 20], trova il valore massimo e trova il valore minino
+
+
+
